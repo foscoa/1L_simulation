95beae4de4ae0241a03a0fe2669385c7880eee33
Fosco <analytics@1lcapital.com>
2023-02-20 15:23:15 +0100

    1st commit

--- daily_returns_simulation.py
+++ daily_returns_simulation.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# commit
+
 # parameters
 mu = 0/10000                    # bps
 sig = 0.006                     # volatility % p.a. np.sqrt(252)
